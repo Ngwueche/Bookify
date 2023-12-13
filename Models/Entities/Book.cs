2c8eaf76f2b155840536d0de98087ddc050ab41e
ngwueche <wisdomngwueche@gmail.com>
2023-12-13 06:59:41 +0100

    Modified the entities

--- Models/Entities/Book.cs
+++ Models/Entities/Book.cs
@@ -11,8 +11,13 @@
         public string Publisher { get; set; } = "";
         public string PhotoUrl { get; set; } = "";
         public string PublicId { get; set; } = "";
+        public string AddedBy { get; set; } = "";
         public int CopiesInLibrary { get; set; }
         public DateTime PublishedDate { get; set; }
+
+        //Navigation Properties
+        public IList<Review> Reviews { get; set; } = new List<Review>();
+        public IList<Rating> Ratings { get; set; } = new List<Rating>();
 
     }
 }
